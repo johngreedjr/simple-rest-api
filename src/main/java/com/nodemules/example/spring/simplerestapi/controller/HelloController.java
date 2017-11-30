8bf5149c36a50481797b01f6c3d256585cea463d
staticsound <johngreedjr@gmail.com>
2017-11-29 23:54:06 -0500

    how far along are we in Prey?

--- src/main/java/com/nodemules/example/spring/simplerestapi/controller/HelloController.java
+++ src/main/java/com/nodemules/example/spring/simplerestapi/controller/HelloController.java
@@ -1,11 +1,14 @@
 package com.nodemules.example.spring.simplerestapi.controller;
 
+import com.github.javafaker.Faker;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+  private static final Faker faker = new Faker();
 
   @RequestMapping("/greet")
   public String sayHello() {
@@ -15,5 +18,10 @@ public class HelloController {
   @RequestMapping("/angrygreet")
   public String sayGoodbye() {
     return "Goodbye World.";
+  }
+
+  @RequestMapping("/prey")
+  public String prey() {
+    return String.format("What percent along does Jay think we are in Prey?  %d%% at least.", faker.number().numberBetween(65,100));
   }
 }
